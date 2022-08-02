dafe772fa879ff9c587437548cf39e4fd28a5f05
hamzahhisham306 <hamzahhisham306@gmail.com>
2022-08-02 18:31:48 +0300

    last

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Form  from './Form';
+import Container from 'react-bootstrap/Container';
+import { Component } from 'react';
+class App extends Component {
+ 
+  render(){
+    return(
+      <>
+        <Container>
+          <Form/>
+        </Container>
+      </>
+    )
+  }
+}
+export default App;
